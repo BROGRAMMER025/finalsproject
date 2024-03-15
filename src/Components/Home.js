9984a43a0764a9e2b15494a8c5f7c136811c8044
allan murigi <allan.murigi@student.moringaschool.com>
2024-03-15 14:19:04 +0300

    updated homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,69 +1,64 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div>
       <Container fluid>
+        {/* Navbar */}
+        <Row>
+          <Col>
+            {/* Navbar component */}
+          </Col>
+        </Row>
+
+        {/* Search Bar */}
+        <Row className="justify-content-center mt-5">
+          <Col md={6}>
+            <Card>
+              <Card.Body>
+                <Form className="d-flex">
+                  <Form.Control type="text" placeholder="Track your package" className="me-2" />
+                  <Button variant="primary" type="submit">
+                    <i className="bi bi-search"></i> {/* Bootstrap Icons search icon */}
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+        {/* Slogan Card */}
+        <Row className="mt-5">
+          <Col>
+            <Card className="bg-dark text-white">
+              <Card.Body className="d-flex justify-content-center align-items-center" style={{ height: '12.5vh' }}>
+                <p className="m-0 fs-4 text-black">Delivering Excellence, One Package at a Time!</p>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+
+        {/* Background Image */}
         <Row>
           <Col>
             <img src="/maarten-van-den-heuvel-246398-unsplash.jpg" alt="background" style={{ width: '100%', height: '100vh', objectFit: 'cover' }} />
           </Col>
         </Row>
-      </Container>
-        <div className="footer-overlay"></div>
-        <div className="footer">
-          <Container fluid>
-            <Row>
-              <Col md={3} className="sb_footer-links-div">
-                <h4>For Business</h4>
-                <p>Employer</p>
-                <p>Health Plan</p>
-                <p>Individual</p>
-              </Col>
-              <Col md={3} className="sb_footer-links-div">
-                <h4>Resources</h4>
-                <p>Resource center</p>
-                <p>Testimonials</p>
-               
-              </Col>
-              <Col md={3} className="sb_footer-links-div">
-                <h4>About us</h4>
-                <p>About us</p>
-                <p>Reviews</p>
-                <p>Ratings</p>
-              </Col>
-              <Col md={3} className="sb_footer-links-div">
-                <h4>Comming Soon on</h4>
-                <p>facebook</p>
-                <p>instsgram</p>
-                <p>X</p>
-              </Col>
-            </Row>
-            <Row>
-              <Col className="sb_footer-below">
-                <div className="sb_footer-copyright">
-                  <p>&copy; {new Date().getFullYear()} SENDIT. All rights reserved.</p>
-                </div>
-                <div className="sb_footer-below-links">
-                  <div>
-                    <p>Terms & Conditions</p>
-                  </div>
-                  <div>
-                    <p>Privacy</p>
-                  </div>
-                  <div>
-                    <p>Security</p>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </div>
-    
 
+        {/* Footer */}
+        <Row className="mt-5">
+          <Col>
+            <Card className="bg-dark text-white">
+              <Card.Body className="d-flex justify-content-center align-items-center" style={{ height: '12.5vh' }}>
+                <p className="mb-0">SendiT 2024. All rights reserved. <i className="bi bi-copyright"></i></p> {/* Bootstrap Icons copyright icon */}
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
