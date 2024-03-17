96a6419463fd24b5c43ae6fda7553a451a71362c
allan murigi <allan.murigi@student.moringaschool.com>
2024-03-17 08:41:36 +0300

    updated

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -14,10 +14,10 @@ const NavBar = () => {
            
             <Nav.Link as={Link} to="/contact" className="mx-4">Contact</Nav.Link> {/* Add Contact link */}
             <Nav.Link as={Link} to="/parcel"  className="mx-4" >Parcel</Nav.Link>
-            <Nav.Link as={Link} to="/Login"  className="mx-4" >Login</Nav.Link>
+           
             <Nav.Link as={Link} to="/Orders"  className="mx-4" >Orders</Nav.Link>
             <Nav.Link as={Link} to="/Ordersupdate"  className="mx-4" >OrdersUpdate</Nav.Link>
-          
+            <Nav.Link as={Link} to="/Login"  className="mx-4" >Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
