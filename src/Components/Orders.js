851d4f98d46430eb487ed0fad06596eb00761ffc
Michael Kiptoo <michaelkiptoo2000@gmail.com>
2024-03-22 01:27:27 +0300

    Responsive Order form

--- src/Components/Orders.js
+++ src/Components/Orders.js
@@ -60,79 +60,81 @@ function Order() {
           <div className="card border-primary" style={{ borderWidth: '2px' }}>
             <div className="card-body">
               <h1 className="text-center mb-4">New Order</h1>
-              <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                  <label htmlFor="name" className="form-label">Name</label>
-                  <input
-                    type='text'
-                    id='name'
-                    placeholder='Order Name'
-                    value={name}
-                    onChange={e => setName(e.target.value)}
-                    className="form-control"
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="description" className="form-label">Description</label>
-                  <textarea
-                    id="description"
-                    placeholder=''
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
-                    required
-                    className="form-control"
-                  ></textarea>
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="pickupLocation" className="form-label">Pickup Location</label>
-                  <input
-                    type='text'
-                    id='pickupLocation'
-                    placeholder='e.g .,Nairobi'
-                    value={pickupLocation}
-                    onChange={e => setPickupLocation(e.target.value)}
-                    className="form-control"
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="destinationLocation" className="form-label">Destination Location</label>
-                  <input
-                    type='text'
-                    id='destinationLocation'
-                    placeholder='e.g ., Nakuru'
-                    value={destinationLocation}
-                    onChange={e => setDestinationLocation(e.target.value)}
-                    className="form-control"
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="weight" className="form-label">Weight (kg)</label>
-                  <input
-                    type='number'
-                    id='weight'
-                    placeholder=''
-                    value={weight}
-                    onChange={e => {
-                      setWeight(e.target.value);
-                      calculatePrice();
-                    }}
-                    className="form-control"
-                    required
-                  />
-                </div>
-                {price && (
+              <div className="form-scroll" style={{ maxHeight: '60vh', overflowY: 'auto' }}>
+                <form onSubmit={handleSubmit}>
                   <div className="mb-3">
-                    <label htmlFor="price" className="form-label">Price (Ksh)</label>
-                    <p className="form-control-static">Ksh. {price}</p>
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input
+                      type='text'
+                      id='name'
+                      placeholder='Order Name'
+                      value={name}
+                      onChange={e => setName(e.target.value)}
+                      className="form-control"
+                      required
+                    />
                   </div>
-                )}
-                <div className="text-center">
-                  <button type='submit' className="btn btn-primary" style={{ transition: 'all 0.3s', fontWeight: 'bold' }}>Create Order</button>
-                </div>
-              </form>
+                  <div className="mb-3">
+                    <label htmlFor="description" className="form-label">Description</label>
+                    <textarea
+                      id="description"
+                      placeholder=''
+                      value={description}
+                      onChange={e => setDescription(e.target.value)}
+                      required
+                      className="form-control"
+                    ></textarea>
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="pickupLocation" className="form-label">Pickup Location</label>
+                    <input
+                      type='text'
+                      id='pickupLocation'
+                      placeholder='e.g .,Nairobi'
+                      value={pickupLocation}
+                      onChange={e => setPickupLocation(e.target.value)}
+                      className="form-control"
+                      required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="destinationLocation" className="form-label">Destination Location</label>
+                    <input
+                      type='text'
+                      id='destinationLocation'
+                      placeholder='e.g ., Nakuru'
+                      value={destinationLocation}
+                      onChange={e => setDestinationLocation(e.target.value)}
+                      className="form-control"
+                      required
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="weight" className="form-label">Weight (kg)</label>
+                    <input
+                      type='number'
+                      id='weight'
+                      placeholder=''
+                      value={weight}
+                      onChange={e => {
+                        setWeight(e.target.value);
+                        calculatePrice();
+                      }}
+                      className="form-control"
+                      required
+                    />
+                  </div>
+                  {price && (
+                    <div className="mb-3">
+                      <label htmlFor="price" className="form-label">Price (Ksh)</label>
+                      <p className="form-control-static">Ksh. {price}</p>
+                    </div>
+                  )}
+                  <div className="text-center">
+                    <button type='submit' className="btn btn-primary" style={{ transition: 'all 0.3s', fontWeight: 'bold' }}>Create Order</button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
