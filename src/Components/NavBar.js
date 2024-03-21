4976f207157d17495d187d039d126583a8931919
Michael Kiptoo <michaelkiptoo2000@gmail.com>
2024-03-21 17:53:40 +0300

    Onclick toggle navbar update

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showLogoutMessage, setShowLogoutMessage] = useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     const accessToken = localStorage.getItem('access_token');
@@ -25,26 +26,27 @@ const NavBar = () => {
     localStorage.removeItem('access_token');
     setIsLoggedIn(false);
     setShowLogoutMessage(true);
+    setExpanded(false); 
     window.location.href = "/";
   };
 
   return (
-    <Navbar expand="lg" className="bg-primary">
-      <Container>
-        <Navbar.Brand as={Link} to="/" className="me-auto">SENDIT</Navbar.Brand>
+    <Navbar expand="lg" className="bg-primary" expanded={expanded} onToggle={() => setExpanded(!expanded)}>
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/" className="me-auto flex-grow-1">SENDIT</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/" className="mx-4">Home</Nav.Link>
-            <Nav.Link as={Link} to="/Orders"  className="mx-4" >Orders</Nav.Link>
-            <Nav.Link as={Link} to="/userparcels"  className="mx-4" >Parcels</Nav.Link>
-            <Nav.Link as={Link} to="/adminparcels"  className="mx-4" >AdminParcels</Nav.Link>
-            <Nav.Link as={Link} to="/AboutUs"  className="mx-4" >AboutUs</Nav.Link>
-            <Nav.Link as={Link} to="/contact" className="mx-4">Contact</Nav.Link> 
+            <Nav.Link as={Link} to="/" className="mx-4" onClick={() => setExpanded(false)}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/Orders" className="mx-4" onClick={() => setExpanded(false)}>Orders</Nav.Link>
+            <Nav.Link as={Link} to="/userparcels" className="mx-4" onClick={() => setExpanded(false)}>Parcels</Nav.Link>
+            <Nav.Link as={Link} to="/adminparcels" className="mx-4" onClick={() => setExpanded(false)}>AdminParcels</Nav.Link>
+            <Nav.Link as={Link} to="/AboutUs" className="mx-4" onClick={() => setExpanded(false)}>AboutUs</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="mx-4" onClick={() => setExpanded(false)}>Contact</Nav.Link>
             {isLoggedIn ? (
               <Nav.Link onClick={handleLogout} className="mx-4">Logout</Nav.Link>
             ) : (
-              <Nav.Link as={Link} to="/Login"  className="mx-4" >Login</Nav.Link>
+              <Nav.Link as={Link} to="/Login" className="mx-4" onClick={() => setExpanded(false)}>Login</Nav.Link>
             )}
           </Nav>
         </Navbar.Collapse>
